ac8fe97a13b079aebe297f07b5caca8928c17087
Elias <emangulo1@gmail.com>
2024-04-19 13:54:05 -0700

    Add time studious page

---
+++ vf/general/time-studious/script.js
@@ -0,0 +1,34 @@
+// Initital settings
+
+let count = 0;
+let prevTime;
+let cycleTime;
+
+let log = [];
+
+document.getElementById("settingsDisplay").innerHTML = ""; // Display settings
+document.getElementById("input").focus(); //Initial focus
+
+// When "Enter" is pressed
+document.getElementById("input").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault;  
+    addCount(Number(document.getElementById("input").value));
+    
+  }
+});
+
+function addCount(input) {
+  cycleTime = (Date.now() - prevTime) / 1000;
+  prevTime = Date.now();
+
+  log.unshift(input + " - " + cycleTime);
+
+  count += 1;
+
+  document.getElementById("group").innerHTML = log.join("<br />");
+  document.getElementById("counter").innerHTML = `Count: ${count}`;
+
+  // Reset input
+  document.getElementById("input").value = "";
+}
