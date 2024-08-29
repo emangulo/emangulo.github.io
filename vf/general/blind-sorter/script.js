34074b166a11a221eb65c6e14fe4d8c828c81ddf
Elias Angulo <emangulo1@gmail.com>
2024-08-28 18:58:37 -0700

    Add blind sorter

---
+++ vf/general/blind-sorter/script.js
@@ -0,0 +1,41 @@
+let skuList = [123, 456];
+
+// Initital settings
+
+let count = 0;
+
+document.getElementById("settingsDisplay").innerHTML = ""; // Display settings
+document.getElementById("input").focus(); //Initial focus
+
+// When "Enter" is pressed
+document.getElementById("input").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault;
+    if (document.getElementById("input").value != "") {
+      getGroupLocation(Number(document.getElementById("input").value));
+    }
+  }
+});
+
+// When UPC is scanned
+function getGroupLocation(inputValue) {
+  let group;
+  document.getElementById("skuDisplay").innerHTML = inputValue;
+
+  let index = skuList.indexOf(inputValue);
+
+  if (index == -1) {
+    skuList.push(inputValue);
+    group = skuList.indexOf(inputValue) + 1;
+  } else {
+    group = index + 1;
+  }
+
+  count += 1;
+
+  document.getElementById("group").innerHTML = group;
+  document.getElementById("counter").innerHTML = `Count: ${count}`;
+
+  // Reset input
+  document.getElementById("input").value = "";
+}
--- vf/index.md
+++ vf/index.md
@@ -1,7 +1,10 @@
 ## VF PROJECTS
 
-### Ontario DC
+### Tools
+- [Blind Sorter](../vf/general/blind-sorter/index.html)
 - [Time Studious](../vf/general/time-studious/index.html)
+
+### Ontario DC
 - [Uvalde Transfers Sorter](../vf/ontario/uv-sorter/index.html)
 
 ### Uvalde DC
